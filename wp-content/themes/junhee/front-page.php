dcb7e23f37bbeccd4011315ba1be875a183a4d34
hoaint <hoaint116@gmail.com>
2015-09-03 17:26:10 +0700

    tam xong

--- wp-content/themes/junhee/front-page.php
+++ wp-content/themes/junhee/front-page.php
@@ -9,4 +9,34 @@
 /** Force full width layout */
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
+add_action('genesis_meta', 'home_genesis_meta');
+/**
+ * Add widget support for homepage. If no widgets active, display the default loop.
+ *
+ */
+function home_genesis_meta()
+{
+    remove_action('genesis_loop', 'genesis_do_loop');
+    add_action('genesis_loop', 'home_loop_helper');
+}
+
+/**
+ * Display widget content for homepage sections.
+ *
+ */
+function home_loop_helper()
+{
+    if (is_active_sidebar('service-section')) {
+        echo '<div class="service-section">';
+        dynamic_sidebar('service-section');
+        echo '</div>';
+    }
+
+    if (is_active_sidebar('body-section')) {
+        echo '<div class="body-section">';
+        dynamic_sidebar('body-section');
+        echo '</div>';
+    }
+}
+
 genesis();
--- wp-content/themes/junhee/lib/theme-views.php
+++ wp-content/themes/junhee/lib/theme-views.php
@@ -10,6 +10,14 @@ remove_action('genesis_after_header', 'genesis_do_subnav');
 // add primary & secondary nav to top of the page
 add_action('genesis_header_right', 'genesis_do_nav');
 
+add_action('genesis_after_header', 'revSliderSection', 5);
+function revSliderSection()
+{
+    if (is_home()) {
+        putRevSlider("banner");
+    }
+}
+
 /**
  * Add images size
  */
@@ -18,3 +26,15 @@ add_image_size('img-thumb-list-widget', 87, 71, true);
 add_image_size('img-thumb-big-archive', 337, 190, true);
 add_image_size('img-thumb-list-archive', 238, 127, true);
 add_image_size('img-thumb-list-small-widget', 75, 75, true);
+
+genesis_register_sidebar(array(
+    'id' => 'service-section',
+    'name' => 'Service Section',
+    'description' => 'This is a service section'
+));
+
+genesis_register_sidebar(array(
+    'id' => 'body-section',
+    'name' => 'Body Section',
+    'description' => 'This is a body section'
+));
